718da60ff90a46d96e440abad9725718e19f558e
Kylee (Kai-ling) Peng <kap038@ucsd.edu>
2019-09-15 09:21:35 +0800

    switching pages now starts at top

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import {  HashRouter } from 'react-router-dom';
 import './index.css';
 import AppRouter from './App'
 import * as serviceWorker from './serviceWorker';
+import ScrollToTop from './components/ScrollToTop';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 ReactDOM.render(
   <HashRouter>
-    <div>
-      <AppRouter routes={AppRouter} />
-    </div>
+    <ScrollToTop>
+      <div>
+        <AppRouter routes={AppRouter} />
+      </div>
+    </ScrollToTop>
   </HashRouter >
   ,
   document.getElementById( 'root' ) );
